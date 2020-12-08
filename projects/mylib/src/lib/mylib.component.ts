2b6d12926031e3a99c6000506a3b0e7f4b46dbcf
Yaroslav Admin <devoto13@gmail.com>
2020-12-08 20:17:33 +0100

    Add failing code

--- projects/mylib/src/lib/mylib.component.ts
+++ projects/mylib/src/lib/mylib.component.ts
@@ -1,20 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, forwardRef, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+const TEST_VALUE_ACCESSOR = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => MylibComponent),
+  multi: true,
+};
 
 @Component({
   selector: 'lib-mylib',
-  template: `
-    <p>
-      mylib works!
-    </p>
-  `,
-  styles: [
-  ]
+  template: ` <p>mylib works!</p> `,
+  styles: [],
+  providers: [TEST_VALUE_ACCESSOR],
 })
-export class MylibComponent implements OnInit {
+export class MylibComponent implements OnInit, ControlValueAccessor {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  writeValue(obj: any): void {
+    throw new Error('Method not implemented.');
+  }
+  registerOnChange(fn: any): void {
+    throw new Error('Method not implemented.');
+  }
+  registerOnTouched(fn: any): void {
+    throw new Error('Method not implemented.');
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    throw new Error('Method not implemented.');
   }
 
+  ngOnInit(): void {}
 }
